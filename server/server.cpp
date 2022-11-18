599efee70af52324ce7063ab86f4a30d9ea49302
nknguyen2112 <nknguyen2112@gmail.com>
2022-11-18 08:02:43 +0700

    non blocking server

--- server/server.cpp
+++ server/server.cpp
@@ -1,198 +1,191 @@
 #include <stdio.h>
-#include <string.h>  
+#include <string.h> //strlen
 #include <stdlib.h>
 #include <errno.h>
-#include <unistd.h>   
-#include <arpa/inet.h>    
+#include <unistd.h> //close
+#include <arpa/inet.h> //close
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <sys/time.h>
+#include <sys/time.h> //FD_SET, FD_ISSET, FD_ZERO macros
+#include <fcntl.h>
 #include <iostream>
-#include <netdb.h>
+#include <string>
 
-#define TRUE   1
+#define TRUE 1
+#define FALSE 0
 #define PORT 54000
 
 using namespace std;
-
+	
 int main(int argc , char *argv[])
 {
-    // Create socket
-    int listening = socket(AF_INET , SOCK_STREAM , 0);
-    if (listening == -1)
-    {
-        cerr << "Can't create a socket! Quitting" << endl;
-        return -1;
-    }
+	int opt = TRUE;
+	int master_socket, // master socket
+		new_socket,
+		client_socket[30],
+		max_clients = 30,
+		activity,
+		valread;
+	sockaddr_in address;
+		
+	char buffer[1025]; //data buffer of 1K
+		
+	//set of socket descriptors
+	fd_set readfds;
+		
+	//a message
+	char *message = "ECHO Daemon v1.0 \r\n";
+	
+	//initialise all client_socket[] to 0 so not checked
+	for (int i = 0; i < max_clients; i++)
+	{
+		client_socket[i] = 0;
+	}
+		
+	//create a master socket
+	if( (master_socket = socket(AF_INET , SOCK_STREAM , 0)) == 0)
+	{
+		perror("socket failed");
+		exit(EXIT_FAILURE);
+	}
+	
+	//set master socket to allow multiple connections
+	if( setsockopt(master_socket, SOL_SOCKET, SO_REUSEADDR, (char *)&opt, sizeof(opt)) < 0 )
+	{
+		perror("setsockopt");
+		exit(EXIT_FAILURE);
+	}
 
-    // Bind socket to TCP port
-    sockaddr_in hint;
-    hint.sin_family = AF_INET;
-    hint.sin_port = htons(PORT);
-    inet_pton(AF_INET, "0.0.0.0", &hint.sin_addr); // 127.0.0.1
-    bind(listening, (sockaddr*)&hint, sizeof(hint));
-    // if (bind(listening, (sockaddr*)&hint, sizeof(hint)) == -1)
-    // {   
-    //     cerr << "Can't bind to IP/port" << endl;
-    //     return -2;
-    // }
+	// non-blocking mode
+	if( fcntl(master_socket, F_SETFL, O_NONBLOCK) < 0 )
+	{
+		perror("fcntl");
+		exit(EXIT_FAILURE);
+	}
+	
+	//type of socket created
+	address.sin_family = AF_INET;
+	address.sin_addr.s_addr = INADDR_ANY;
+	address.sin_port = htons( PORT );
+		
+	//bind the socket to port
+	if (bind(master_socket, (sockaddr *)&address, sizeof(address))<0)
+	{
+		perror("bind failed");
+		exit(EXIT_FAILURE);
+	}
+	printf("Listener on port %d \n", PORT);
+		
+	//try to specify maximum of 3 pending connections for the master socket
+	if (listen(master_socket, 3) < 0)
+	{
+		perror("listen");
+		exit(EXIT_FAILURE);
+	}
+		
+	//accept the incoming connection
+	int addrlen = sizeof(address);
+	puts("Waiting for connections ...");
+		
+	while(TRUE)
+	{
+		//clear the socket set
+		FD_ZERO(&readfds);
+	
+		//add master socket to set
+		FD_SET(master_socket, &readfds);
+		int max_sd = master_socket;
 
-    // Listen for incoming connections
-    if (listen(listening, SOMAXCONN) == -1)
-    {
-        cerr << "Can't listen!" << endl;
-        return -3;
-    } 
-
-    // Accept incoming connection
-    sockaddr_in client;
-    socklen_t clientSize = sizeof(client);
-    char host[NI_MAXHOST];      // Client's remote name
-    char service[NI_MAXSERV];   // Service (i.e. port) the client is connect on
-    int clientSocket = accept(listening, (sockaddr*)&client, &clientSize);
-    if (clientSocket == -1)
-    {
-        cerr << "Problem with client connecting!" << endl;
-        return -4;
-    }
-    close(listening);
-
-    // Close listening socket
-    memset(host, 0, NI_MAXHOST); // same as memset(host, 0, NI_MAXHOST);
-    memset(service, 0, NI_MAXSERV);
-
-    int result = getnameinfo((sockaddr*)&client, 
-                            sizeof(client), 
-                            host, 
-                            NI_MAXHOST, 
-                            service, 
-                            NI_MAXSERV, 
-                            0);
-    if (result) 
-    {
-        cout << host << " connected on " << service << endl;
-    } 
-    else 
-    {
-        inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
-        cout << host << " connected on " << ntohs(client.sin_port) << endl;
-    }
-
-    // While loop: accept and echo message back to client
-    char buf[4096];
-    while (true)
-    {
-        // Clear buffer
-        memset(buf, 0, 4096); 
-        // Wait for client to send data
-        int bytesReceived = recv(clientSocket, buf, 4096, 0);
-        if (bytesReceived == -1)
-        {
-            cerr << "Error in recv(). Quitting" << endl;
-            break;
-        }
-        if (bytesReceived == 0)
-        {
-            cout << "Client disconnected " << endl;
-            break;
-        }
-        // Echo message back to client
-        send(clientSocket, "Hello", bytesReceived + 1, 0);
-    }
-    // Close socket
-    close(clientSocket);
-
-    return 0;
+		//add child sockets to set
+		for ( int i = 0 ; i < max_clients ; i++)
+		{
+			//socket descriptor
+			int sd = client_socket[i];
+				
+			//if valid socket descriptor then add to read list
+			if(sd > 0)
+				FD_SET(sd , &readfds);
+				
+			//highest file descriptor number, need it for the select function
+			if(sd > max_sd)
+				max_sd = sd;
+		}
+	
+		//wait for an activity on one of the sockets, timeout is NULL, so wait indefinitely
+		activity = select( max_sd + 1 , &readfds , NULL , NULL , NULL);
+	
+		if ((activity < 0) && (errno!=EINTR))
+		{
+			printf("select error");
+		}
+			
+		//If something happened on the master socket, then its an incoming connection
+		if (FD_ISSET(master_socket, &readfds))
+		{
+			if ((new_socket = accept(master_socket,
+					(sockaddr *)&address, (socklen_t*)&addrlen))<0)
+			{
+				perror("accept");
+				exit(EXIT_FAILURE);
+			}
+			
+			//inform user of socket number - used in send and receive commands
+			printf("New connection , socket fd is %d , ip is : %s , port : %d \n", new_socket, inet_ntoa(address.sin_addr), ntohs(address.sin_port));
+		
+			//send new connection greeting message
+			if( send(new_socket, message, strlen(message), 0) != strlen(message) )
+			{
+				perror("send");
+			}
+				
+			puts("Welcome message sent successfully");
+				
+			//add new socket to array of sockets
+			for (int i = 0; i < max_clients; i++)
+			{
+				//if position is empty
+				if( client_socket[i] == 0 )
+				{
+					client_socket[i] = new_socket;
+					printf("Adding to list of sockets as %d\n" , i);
+						
+					break;
+				}
+			}
+		}
+			
+		//else its some IO operation on some other socket
+		for (int i = 0; i < max_clients; i++)
+		{
+			int sd = client_socket[i];
+				
+			if (FD_ISSET( sd , &readfds))
+			{
+				//Check if it was for closing , and also read the
+				//incoming message
+				if ((valread = read( sd , buffer, 1024)) == 0)
+				{
+					//Somebody disconnected , get his details and print
+					getpeername(sd , (sockaddr*)&address, (socklen_t*)&addrlen);
+					printf("Host disconnected , ip %s , port %d \n" , inet_ntoa(address.sin_addr) , ntohs(address.sin_port));
+						
+					//Close the socket and mark as 0 in list for reuse
+					close( sd );
+					client_socket[i] = 0;
+				}
+					
+				//Echo back the message that came in
+				else
+				{
+					//set the string terminating NULL byte on the end
+					//of the data read
+					buffer[valread] = '\0';
+					send(sd , buffer , strlen(buffer) , 0 );
+				}
+			}
+		}
+	}
+		
+	return 0;
 }
-
-
-// #include <iostream>
-// #include <sys/types.h>
-// #include <unistd.h>
-// #include <sys/socket.h>
-// #include <netdb.h>
-// #include <arpa/inet.h>
-// #include <string.h>
-// #include <string>
- 
-// using namespace std;
- 
-// int main()
-// {
-//     // Create a socket
-//     int listening = socket(AF_INET, SOCK_STREAM, 0);
-//     if (listening == -1)
-//     {
-//         cerr << "Can't create a socket! Quitting" << endl;
-//         return -1;
-//     }
- 
-//     // Bind the ip address and port to a socket
-//     sockaddr_in hint;
-//     hint.sin_family = AF_INET;
-//     hint.sin_port = htons(54000);
-//     inet_pton(AF_INET, "0.0.0.0", &hint.sin_addr);
- 
-//     bind(listening, (sockaddr*)&hint, sizeof(hint));
- 
-//     // Tell Winsock the socket is for listening
-//     listen(listening, SOMAXCONN);
- 
-//     // Wait for a connection
-//     sockaddr_in client;
-//     socklen_t clientSize = sizeof(client);
- 
-//     int clientSocket = accept(listening, (sockaddr*)&client, &clientSize);
- 
-//     char host[NI_MAXHOST];      // Client's remote name
-//     char service[NI_MAXSERV];   // Service (i.e. port) the client is connect on
- 
-//     memset(host, 0, NI_MAXHOST); // same as memset(host, 0, NI_MAXHOST);
-//     memset(service, 0, NI_MAXSERV);
- 
-//     if (getnameinfo((sockaddr*)&client, sizeof(client), host, NI_MAXHOST, service, NI_MAXSERV, 0) == 0)
-//     {
-//         cout << host << " connected on port " << service << endl;
-//     }
-//     else
-//     {
-//         inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
-//         cout << host << " connected on port " << ntohs(client.sin_port) << endl;
-//     }
- 
-//     // Close listening socket
-//     close(listening);
- 
-//     // While loop: accept and echo message back to client
-//     char buf[4096];
- 
-//     while (true)
-//     {
-//         memset(buf, 0, 4096);
- 
-//         // Wait for client to send data
-//         int bytesReceived = recv(clientSocket, buf, 4096, 0);
-//         if (bytesReceived == -1)
-//         {
-//             cerr << "Error in recv(). Quitting" << endl;
-//             break;
-//         }
- 
-//         if (bytesReceived == 0)
-//         {
-//             cout << "Client disconnected " << endl;
-//             break;
-//         }
- 
-//         cout << string(buf, 0, bytesReceived) << endl;
- 
-//         // Echo message back to client
-//         send(clientSocket, buf, bytesReceived + 1, 0);
-//     }
- 
-//     // Close the socket
-//     close(clientSocket);
- 
-//     return 0;
-// }
